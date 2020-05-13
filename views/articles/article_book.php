eb5a02d452e004142cdeb826b5535252e653491b
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.tech>
2020-05-13 17:22:45 +0300

    Added image and desc nullability

--- views/articles/article_book.php
+++ views/articles/article_book.php
@@ -1,11 +1,17 @@
 <article>
 
     <div class="item corners elevation">
-        <img src="<?= $params['book']->image ?>" alt="book image">
+        <?php if ($params['book']->image): ?>
+            <img src="<?= $params['book']->image ?>" alt="book image">
+        <?php endif ?>
         <div class="item-content">
             <div class="name"><?= $params['book']->name ?></div>
             <div class="description"><?= $params['book']->author ?></div>
             <div class="price"><?= $params['book']->price ?></div>
+            <br>
+            <?php if ($params['book']->description): ?>
+            <div><?= $params['book']->description ?></div>
+            <?php endif ?>
         </div>
     </div>
 
--- views/articles/article_books.php
+++ views/articles/article_books.php
@@ -1,6 +1,5 @@
 <article>
     <div class="category">
-<!--        <a class="chip chip-active" href="books_detective.html">Детектив</a>-->
 
         <?php foreach ($params['categories'] as $item): ?>
             <a
@@ -16,12 +15,16 @@
         <?php foreach ($params['books'] as $item): ?>
 
             <a class="item corners elevation" href="/book/<?= $item->id ?>">
-            <img src="<?= $item->image ?>" alt="book image">
-            <div class="item-content">
-                <div class="name"><?= $item->name ?></div>
-                <div class="description"><?= $item->author ?></div>
-                <div class="price"><?= $item->price ?></div>
-            </div>
+
+                <?php if ($item->image): ?>
+                    <img src="<?= $item->image ?>" alt="book image">
+                <?php endif ?>
+
+                <div class="item-content">
+                    <div class="name"><?= $item->name ?></div>
+                    <div class="description"><?= $item->author ?></div>
+                    <div class="price"><?= $item->price ?></div>
+                </div>
             </a>
 
         <?php endforeach ?>
